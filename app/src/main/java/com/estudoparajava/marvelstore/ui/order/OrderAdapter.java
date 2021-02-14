ffe47c6a91118aca1ddc44bdcf2216081c53c019
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2021-02-14 18:12:29 -0300

    Layout de pedidos pronto

---
+++ app/src/main/java/com/estudoparajava/marvelstore/ui/order/OrderAdapter.java
@@ -0,0 +1,52 @@
+package com.estudoparajava.marvelstore.ui.order;
+
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.estudoparajava.marvelstore.R;
+
+public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.MyViewHolderOrder> {
+
+    @NonNull
+    @Override
+    public MyViewHolderOrder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemLista = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.adapter_order_list, parent, false);
+
+        return new MyViewHolderOrder(itemLista);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull OrderAdapter.MyViewHolderOrder holder, int position) {
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public class MyViewHolderOrder extends  RecyclerView.ViewHolder{
+
+
+        public MyViewHolderOrder(View itemView) {
+            super(itemView);
+
+
+        }
+    }
+
+
+
+
+
+}
--- app/src/main/java/com/estudoparajava/marvelstore/ui/order/OrderFragment.java
+++ app/src/main/java/com/estudoparajava/marvelstore/ui/order/OrderFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,12 +12,17 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.estudoparajava.marvelstore.R;
+import com.estudoparajava.marvelstore.ui.cart.CartAdpater;
 
 public class OrderFragment extends Fragment {
 
     private OrderViewModel orderViewModel;
+    private RecyclerView recyclerViewOrder;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -24,11 +30,31 @@ public class OrderFragment extends Fragment {
         orderViewModel =
                 new ViewModelProvider(this).get(OrderViewModel.class);
         View root = inflater.inflate(R.layout.fragment_order, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
+        //final TextView textView = root.findViewById(R.id.text_gallery);
+
+
+        recyclerViewOrder = root.findViewById(R.id.recycleViewOrder);
+
+
+        //Adapter configuration
+        OrderAdapter orderAdapter = new OrderAdapter();
+
+
+
+        //RecyclerView configuration
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerViewOrder.setLayoutManager(layoutManager);
+        recyclerViewOrder.setHasFixedSize(true);
+        recyclerViewOrder.addItemDecoration(new DividerItemDecoration(getContext(), LinearLayout.VERTICAL));
+        recyclerViewOrder.setAdapter(orderAdapter);
+
+
+
         orderViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
+
             }
         });
         return root;
